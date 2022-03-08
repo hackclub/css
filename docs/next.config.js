75d7f09bb6fe721619cb29237411b65819fcad1a
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-03-08 17:25:48 +0800

    Restructure Repo

---
+++ docs/next.config.js
@@ -0,0 +1,16 @@
+const nextConfig = {
+  trailingSlash: true,
+  async redirects() {
+    return [
+      { source: '/:file.css', destination: `https://cdn.jsdelivr.net/gh/hackclub/css@${process.env.VERCEL_GIT_COMMIT_SHA ? process.env.VERCEL_GIT_COMMIT_SHA : 'main'}/:file.css`, permanent: false },
+      { source: '/:slug/:file.css', destination: 'https://cdn.jsdelivr.net/gh/hackclub/css@:slug/:file.css', permanent: false },
+    ]
+  },
+  async rewrites() {
+    return [
+      { source: '/', destination: '/index.html' },
+    ]
+  },
+}
+
+module.exports = nextConfig
