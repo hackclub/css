6b52ce805a6f8ae05bd62b5fd2f5632b443bcd94
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-03-10 19:29:38 +0800

    Symlink

--- docs/next.config.js
+++ docs/next.config.js
@@ -2,7 +2,7 @@ const nextConfig = {
   trailingSlash: true,
   async redirects() {
     return [
-      { source: '/:file.css', destination: `https://cdn.jsdelivr.net/gh/hackclub/css@${process.env.VERCEL_GIT_COMMIT_SHA ? process.env.VERCEL_GIT_COMMIT_SHA : 'main'}/:file.css`, permanent: false },
+      { source: '/:file.css', destination: process.env.VERCEL_GIT_COMMIT_SHA ? `https://cdn.jsdelivr.net/gh/hackclub/css@${process.env.VERCEL_GIT_COMMIT_SHA ? process.env.VERCEL_GIT_COMMIT_SHA : 'main'}/:file.css` : ':file.css', permanent: false },
       { source: '/:slug/:file.css', destination: 'https://cdn.jsdelivr.net/gh/hackclub/css@:slug/:file.css', permanent: false },
     ]
   },
