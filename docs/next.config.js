338b131269f0d7e09caae59db533f88564a16cf9
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-03-10 19:38:13 +0800

    Make it possible to do local dev!

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,16 +1,28 @@
+let productionRedirects = [
+  {
+    source: "/:file.css",
+    destination: `https://cdn.jsdelivr.net/gh/hackclub/css@${
+      process.env.VERCEL_GIT_COMMIT_SHA
+        ? process.env.VERCEL_GIT_COMMIT_SHA
+        : "main"
+    }/:file.css`,
+    permanent: false,
+  },
+  {
+    source: "/:slug/:file.css",
+    destination: "https://cdn.jsdelivr.net/gh/hackclub/css@:slug/:file.css",
+    permanent: false,
+  },
+];
+
 const nextConfig = {
   trailingSlash: true,
   async redirects() {
-    return [
-      { source: '/:file.css', destination: `https://cdn.jsdelivr.net/gh/hackclub/css@${process.env.VERCEL_GIT_COMMIT_SHA ? process.env.VERCEL_GIT_COMMIT_SHA : 'main'}/:file.css`, permanent: false },
-      { source: '/:slug/:file.css', destination: 'https://cdn.jsdelivr.net/gh/hackclub/css@:slug/:file.css', permanent: false },
-    ]
+    return process.env.VERCEL_GIT_COMMIT_SHA ? productionRedirects : [];
   },
   async rewrites() {
-    return [
-      { source: '/', destination: '/index.html' },
-    ]
+    return [{ source: "/", destination: "/index.html" }];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
